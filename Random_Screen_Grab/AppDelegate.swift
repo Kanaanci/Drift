0855f01ed1c8a9dbc9975a79bf56da7d9a602d3e
Kanaan Irwin <kanaanci@yahoo.com>
2018-10-07 20:29:31 -0400

    Initial Push

---
+++ Random_Screen_Grab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Random_Screen_Grab
+//
+//  Created by Kanaan Irwin on 10/3/18.
+//  Copyright © 2018 Kanaan Irwin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
